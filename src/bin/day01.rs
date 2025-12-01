dbaa2af08560587b708e09bb5016fe1fde96ebbf
Ihtasham C <ihtasham.chaudhry@paconsulting.com>
2025-12-01 21:03:09 +0000

    Day 01 cleanup

--- src/bin/day01.rs
+++ src/bin/day01.rs
@@ -1,6 +1,7 @@
 use aoc25::read_lines;
 
 // Day 1: Secret Entrance
+// Output: Solution -> Part1: 1191 || Part2: 6858
 
 pub fn main() {
     let input = include_str!("../../inputs/day01.txt");
@@ -16,17 +17,10 @@ fn solve_part1(input: &Vec<&str>) -> i32 {
     let mut result = 0;
     let mut pos = 50;
 
+    // Simple solution of checking number of 0 "clicks" in total at each move
     for line in input {
         let amount: i32 = line[1..].parse().unwrap();
-
-        if line.starts_with("R") {
-            // + operator
-            pos = (pos + amount).rem_euclid(100);
-        } else {
-            // - operator
-            pos = (pos - amount).rem_euclid(100);
-        }
-
+        pos = (pos + dir(line) * amount).rem_euclid(100);
         if pos == 0 {
             result += 1;
         }
@@ -40,13 +34,13 @@ fn solve_part2(input: &Vec<&str>) -> i32 {
     let mut result = 0;
     let mut pos: i32 = 50;
 
+    // Not efficient but since the input isn't huge it's fine
+    // Looping through each "click" and finding the amount of 0 hits
+    // Still O(n)
     for line in input {
-        let dir = line.chars().next().unwrap();
         let amount: i32 = line[1..].trim().parse().unwrap();
-        let step = if dir == 'R' { 1 } else { -1 };
-
         for _ in 0..amount {
-            pos = (pos + step).rem_euclid(100);
+            pos = (pos + dir(line)).rem_euclid(100);
 
             if pos == 0 {
                 result += 1;
@@ -55,4 +49,13 @@ fn solve_part2(input: &Vec<&str>) -> i32 {
     }
 
     result
+}
+
+// R = 1, L = -1
+fn dir(line: &str) -> i32 {
+    if line.chars().next().unwrap() == 'R' {
+        1
+    } else {
+        -1
+    }
 }
